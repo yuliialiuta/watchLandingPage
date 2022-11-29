ff319ac94173655d206f17e9c562b59ea8375c78
yuliialiuta <kirstya.julia@gmail.com>
2022-11-29 13:41:41 +0200

    add swiper

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  freeMode: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  mousewheel: true,
+});
